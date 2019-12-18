7abbce87a1d887bd00cc8f59d1a81c79dbde638d
wangxiuchun <644511549@qq.com>
2019-12-18 21:46:04 +0800

    0

--- src/test/java/com/qyerp/QyerpApplicationTests.java
+++ src/test/java/com/qyerp/QyerpApplicationTests.java
@@ -1,13 +1,20 @@
 package com.qyerp;
 
+import com.qyerp.dao.ErpSysUserDao;
+import com.qyerp.entity.ErpSysUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class QyerpApplicationTests {
-
+    @Resource
+    private ErpSysUserDao erpSysUserDao;
     @Test
-    void contextLoads() {
+    void contextLoads(int id) {
+        erpSysUserDao.queryById(1);
+        System.out.println(erpSysUserDao.queryById(1));
     }
 
 }
